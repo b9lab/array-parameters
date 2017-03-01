2deb4e0aff6894e21c8ba7c8511bacd96908b60f
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-03-01 12:44:01 +0000

    Touch-up.

--- test/passedOnStruct.js
+++ test/passedOnStruct.js
@@ -12,9 +12,9 @@ contract('PassedOnStruct', accounts => {
             })
             .then(variableLength => {
                 assert.strictEqual(variableLength.length, 2, "should only have 2 values");
-                assert.strictEqual(variableLength[0].toNumber(), 1, "should be get the 1st fixed length");
+                assert.strictEqual(variableLength[0].toNumber(), 1, "should get the 1st fixed length");
                 // In other words, the variable length field is missing
-                assert.strictEqual(variableLength[1].toNumber(), 4, "should be get the 2st fixed length");
+                assert.strictEqual(variableLength[1].toNumber(), 4, "should get the 2st fixed length");
             });
 
     });
